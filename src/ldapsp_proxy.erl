eefbd57f8be7cd5263c1e4f4a16976beafc9dcba
Guido Günther <agx@sigxcpu.org>
2017-02-16 08:07:41 +0100

    Add license boilerplate
    (althought it's sometimes longer than the actual code)

--- src/ldapsp_proxy.erl
+++ src/ldapsp_proxy.erl
@@ -1,11 +1,21 @@
-%%%-------------------------------------------------------------------
-%%% @author Guido <agx@sigxcpu.org>
-%%% @copyright (C) 2016, Guido Günther
-%%% @doc
-%%%
-%%% @end
-%%% Created : 16 Aug 2016 by Guido <agx@sigxcpu.org>
-%%%-------------------------------------------------------------------
+%%-------------------------------------------------------------------
+%% This file is part of ldapsp.
+%%
+%% Copyright (C) 2016 Guido Günther <agx@sigxcpu.org>
+%%
+%% ldapsp is free software: you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation, either version 3 of the License, or
+%% (at your option) any later version.
+%%
+%% ldapsp is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License
+%% along with ldapsp.  If not, see <http://www.gnu.org/licenses/>.
+%%-------------------------------------------------------------------
 -module(ldapsp_proxy).
 
 -export([add_host/3,
